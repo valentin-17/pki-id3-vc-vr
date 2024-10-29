5ab6d3fc9293f2f98468edb778bd10746dbaeeb2
vatschki <valentin.christ@gmx.net>
2024-10-29 12:01:37 +0100

    implemented EqualWidthDiscretization.maxValueminValue, EqualWidthDiscretization.minValue, EqualWidthDiscretization.binWidth, EqualWidthDiscretization.discretize

--- src/main/java/de/uni_trier/wi2/pki/preprocess/EqualWidthDiscretization.java
+++ src/main/java/de/uni_trier/wi2/pki/preprocess/EqualWidthDiscretization.java
@@ -1,5 +1,6 @@
 package de.uni_trier.wi2.pki.preprocess;
 
+import javax.lang.model.element.Element;
 import java.io.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,10 +20,53 @@ public class EqualWidthDiscretization extends BinningDiscretizer {
      * @param attributeId  The ID of the attribute to discretize.
      * @return the list of discretized examples.
      */
+
     public List<Object[]> discretize(int numberOfBins, List<Object[]> examples, int attributeId) {
 
+        if (examples == null || examples.isEmpty()) {
+            throw new IllegalArgumentException("The list of examples is empty.");
+        }
+
+        List<Object[]> outputList = new ArrayList<>();
+
+
+        int binIndex = (int) binWidth(minValue(examples, attributeId), maxValue(examples, attributeId), numberOfBins);
+
+        //noch nicht fertig da fehlt noch woher weis du wann binIndex erreicht ist und du musst das ganz Object Array mit in die neue liste packen
+        for (Object[] example : examples) {
+            example[attributeId] = binIndex;
+            outputList.add(example);
+        }
 
         return outputList;
     }
+
+    private double minValue(List<Object[]> examples, int attributeId) {
+        double min = Double.MAX_VALUE;
+        for (Object[] example : examples) {
+            double value = (double) example[attributeId];
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    private double maxValue(List<Object[]> examples, int attributeId) {
+        double max = Double.MIN_VALUE;
+        for (Object[] example : examples) {
+            double value = (double) example[attributeId];
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    private double binWidth(double min, double max, int numberOfBins) {
+        return (max - min) / numberOfBins;
+    }
+
+
 
 }
