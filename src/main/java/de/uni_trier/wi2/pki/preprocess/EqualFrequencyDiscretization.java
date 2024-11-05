7aed14bb2af0e36405460a882f90d6730f9b335c
koiworks <s4varosa@uni-trier.de>
2024-11-05 15:44:46 +0100

    fixed EqualFrequencyDiscretization with naming

--- src/main/java/de/uni_trier/wi2/pki/preprocess/EqualFrequencyDiscretization.java
+++ src/main/java/de/uni_trier/wi2/pki/preprocess/EqualFrequencyDiscretization.java
@@ -2,6 +2,7 @@ package de.uni_trier.wi2.pki.preprocess;
 
 import de.uni_trier.wi2.pki.util.Helpers;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,46 +22,65 @@ public class EqualFrequencyDiscretization extends BinningDiscretizer {
     public List<Object[]> discretize(int numberOfBins, List<Object[]> examples, int attributeId) {
         int numberOfExamples = examples.size();
         int numberOfExamplesPerBin = numberOfExamples / numberOfBins;
-        String binName = "";
 
         /* sort the given examples by the given attribute */
         List<Object[]> sortedExamples = examples.stream()
-                           .sorted(Comparator.comparing(o -> Double.parseDouble(o[attributeId].toString()))).toList();
+                .sorted(Comparator.comparing(o -> Double.parseDouble(o[attributeId].toString())))
+                .toList();
 
-        System.out.println("Sorted List:");
-        Helpers.printData(sortedExamples);
+        /* create bin names */
+        String[] binNames = createBinNames(sortedExamples, attributeId, numberOfExamplesPerBin, numberOfBins);
 
         /* loop through the sorted examples and assign them to bins */
         for (int i = 0; i < numberOfBins; i++) {
             int finalI = i;
-            String[] binNames = createBinNames(examples, finalI, attributeId, numberOfExamplesPerBin, numberOfBins);
 
             /* assign numberOfExamplesPerBin examples to the current bin */
             sortedExamples.stream()
                           .skip((long) i * numberOfExamplesPerBin)                                                   /* skip the first n examples from the previous bin */
-                          .limit(i == (numberOfBins - 1) ? examples.size() - 1 : numberOfExamplesPerBin)                /* leave the right border open if the last bin is reached */
+                          .limit(calculateEndIndex(i, finalI, numberOfBins, numberOfExamplesPerBin, sortedExamples))    /* limit the stream to n examples */
                           .forEach(example -> example[attributeId] = binNames[finalI]);
         }
 
+        System.out.println("Successfully discretized data into " + numberOfBins + " bins: " + Arrays.toString(binNames));
         return sortedExamples;
     }
 
-    private String[] createBinNames(List<Object[]> examples, int finalI, int attributeId,
-                                    int numberOfExamplesPerBin, int numberOfBins) {
+    /**
+     * Calculates the end index of the bin.
+     * If the current bin is the last bin, the end index is the last example in the list.
+     *
+     * @param i                      The current bin index.
+     * @param finalI                 The final bin index.
+     * @param numberOfBins           The number of bins.
+     * @param numberOfExamplesPerBin The number of examples per bin.
+     * @param sortedExamples         The list of examples to discretize.
+     * @return the end index of the bin.
+     */
+    private long calculateEndIndex(int i, int finalI, int numberOfBins, int numberOfExamplesPerBin, List<Object[]> sortedExamples) {
+        return (i == numberOfBins - 1) ? sortedExamples.size() - 1 : (long) (finalI + 1) * numberOfExamplesPerBin - 1;
+    }
+
+    /**
+     * Creates the names for the bins.
+     *
+     * @param examples               The list of examples to discretize.
+     * @param attributeId            The ID of the attribute to discretize.
+     * @param numberOfExamplesPerBin The number of examples per bin.
+     * @param numberOfBins           The number of bins.
+     * @return the array of bin names.
+     */
+    private String[] createBinNames(List<Object[]> examples, int attributeId, int numberOfExamplesPerBin, int numberOfBins) {
         String[] binNames = new String[numberOfBins];
-        int startIdx = finalI * numberOfExamplesPerBin;                                                                 /* determine the value of the starting example */
-        int endIdx = finalI == (numberOfBins - 1)                                                                       /* determine the value of the last example, */
-                               ? examples.size() - 1                                                                    /* if this is the last bin extend the range */
-                               : numberOfExamplesPerBin;                                                                /* to include all examples remaining */
-        String startVal = examples.get(startIdx)[attributeId].toString();
-        String endVal = examples.get(endIdx)[attributeId].toString();
 
         for (int i = 0; i < numberOfBins; i++) {
-            binNames[i] = ("Bin " + i + ": ").concat("[")                                                                             /* build the bin name */
-                                             .concat(String.valueOf(startVal))
-                                             .concat(", ")
-                                             .concat(String.valueOf(endVal))
-                                             .concat("]");;
+            int startIdx = i * numberOfExamplesPerBin;                                                                  /* determine the index of the starting example */
+            int endIdx = (i == numberOfBins - 1) ? examples.size() - 1                                                  /* determine the index of the last example, */
+                          : (i + 1) * numberOfExamplesPerBin - 1;                                                       /* if this is the last bin extend the range to include all examples remaining*/
+            String startVal = examples.get(startIdx)[attributeId].toString();
+            String endVal = examples.get(endIdx)[attributeId].toString();
+
+            binNames[i] = String.format("BinId %d: [%s, %s]", i, startVal, endVal);
         }
 
         return binNames;
